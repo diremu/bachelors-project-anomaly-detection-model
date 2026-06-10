34d10b06b2442b170319eecfed267a71672673c3
Dbax <123155596+diremu@users.noreply.github.com>
2026-06-10 02:30:24 +0000

    refactor(styling): updated tailwind

--- console/src/App.tsx
+++ console/src/App.tsx
@@ -1,3 +1,4 @@
+import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import DashboardLayout from './components/Dashboard';
 import SystemOverview from './components/SystemOverview';
