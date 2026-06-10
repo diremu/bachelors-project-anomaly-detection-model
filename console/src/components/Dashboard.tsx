88bee3bea4cca93060ecfc81c5609cb3fa6c58ba
Dbax <123155596+diremu@users.noreply.github.com>
2026-06-10 02:41:29 +0000

    fix(dashboard): static time made dynamic

--- console/src/components/Dashboard.tsx
+++ console/src/components/Dashboard.tsx
@@ -1,7 +1,13 @@
 import Sidebar from './Sidebar';
 import { Outlet } from 'react-router-dom';
+import {useEffect, useState} from 'react';
 
 export default function Dashboard() {
+    const [currentTime, setCurrentTime] = useState(new Date());
+    useEffect(() => {
+        const timer = setInterval(() => setCurrentTime(new Date()), 1000);
+    return () => clearInterval(timer);
+    }, [])
   return (
     <div className="flex h-screen w-full bg-[#0d1117] text-gray-200 font-poppins overflow-hidden">
       <div className="absolute top-0 w-full h-16 flex items-center justify-between px-6 bg-[#161b22] border-b border-gray-800 z-10">
@@ -11,7 +17,7 @@ export default function Dashboard() {
           </span>
         </div>
         <div className="flex items-center gap-4">
-          <span className="text-sm text-gray-400">10:14:52 AM</span>
+          <span className="text-sm text-gray-400">{currentTime.toLocaleTimeString('en-us',{hour12: true})}</span>
           <div className="px-3 py-1 bg-green-900/30 text-green-400 rounded-full text-xs font-medium flex items-center gap-2">
             <span className="w-2 h-2 bg-green-500 rounded-full animate-pulse"></span>
             LIVE
